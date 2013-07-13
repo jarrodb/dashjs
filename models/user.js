364e63d0837feb1b92d48a905000d5680551dc55
Jarrod Baumann <jarrod@theos.org>
2013-07-12 23:14:21 -0500

    Basic User model for storing user information

---
+++ models/user.js
@@ -0,0 +1,33 @@
+/*
+ * User
+ */
+
+var level = require('../libs/level');
+var bcrypt = require('bcrypt');
+
+var User = level.Model({
+  fullname: {type: String},
+  username: {type: String}, //, required: true},
+  password: {type: String}
+},{
+  key: 'username',
+  set_password: function(password) {
+    this.password = bcrypt.hashSync(password, 8);
+  },
+  authenticate: function(username, password, cb) {
+    level.db.get(this.key+'~'+username, function(err, user) {
+      if (err) return cb(err);
+
+      var user = JSON.parse(user);
+      if (bcrypt.compareSync(password, user.password))
+        return cb(null, user);
+      return cb(new Error("invalid authentication"));
+    });
+  },
+  validate: function() {
+    if (!this.password) return new Error('password required');
+  }
+});
+
+level.model('User', User);
+
